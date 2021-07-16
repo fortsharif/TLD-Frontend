2b6f4252097be5a76c95d4b2987786636456bac7
fortsharif <huseinsharif.96@gmail.com>
2021-07-16 16:53:42 +0100

    centered card for UI

--- src/application/Application.js
+++ src/application/Application.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
-
+import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap'
+import './Application.css'
 
 const MyApplication = (props) => {
 
 
+
+    const email = localStorage.getItem('email')
     const [application, setApplication] = useState({})
     const [loading, setLoading] = useState(true)
-    const [image, setImage] = useState(null)
+    const [image, setImage] = useState(process.env.PUBLIC_URL + `/${email}.png`)
 
 
 
@@ -38,15 +41,33 @@ const MyApplication = (props) => {
         getApplication()
     }, [])
 
-    return <div className='card text-center'>
-        {/* <img src={process.env.PUBLIC_URL + '/noimage.png'} alt="" /> */}
-        <h1>{application.status}</h1>
-        <h1>{application.name}</h1>
-        <h1>{application.occupation}</h1>
-        <h1>{application.number}</h1>
-        <h1>{application.address}</h1>
-        {loading ? <h1>Loading...</h1> : <h1>{application.date.slice(0, 10)}</h1>}
-    </div>
+    return (<>
+
+        <Card border='dark' className="hmm">
+            <Card.Header>{loading ? <h1>hehe</h1> : application.status.toUpperCase()}</Card.Header>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+                <Card.Title>Status of your application</Card.Title>
+                <Card.Text>
+                    Hello, {application.name} your application status is currently; {application.status}. More information about what you submitted with your application can be found below...
+                </Card.Text>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>Occupation: {application.occupation}</ListGroupItem>
+                    <ListGroupItem>Number: {application.number}</ListGroupItem>
+                    <ListGroupItem>Address: {application.address}</ListGroupItem>
+                </ListGroup>
+
+
+                <Button variant="dark">Back to dashboard</Button>
+
+            </Card.Body>
+            <Card.Footer className="text-muted">{loading ? < Card.Text > loading </Card.Text> : <Card.Text> Submitted on {application.date.slice(0, 10)} </Card.Text>}</Card.Footer>
+        </Card >
+
+    </>
+    )
+
+
 
 }
 

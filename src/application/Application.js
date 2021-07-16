2caab9dcfb943e9e1c50ea8f0875f20d618c301d
fortsharif <huseinsharif.96@gmail.com>
2021-07-16 15:54:35 +0100

    bug with loading in data fixed

--- src/application/Application.js
+++ src/application/Application.js
@@ -5,6 +5,10 @@ const MyApplication = (props) => {
 
 
     const [application, setApplication] = useState({})
+    const [loading, setLoading] = useState(true)
+    const [image, setImage] = useState(null)
+
+
 
     const getApplication = async () => {
         const url = 'http://localhost:5000/api/v1/applications/status'
@@ -20,7 +24,9 @@ const MyApplication = (props) => {
 
         if (status === 200) {
             const data = await response.json()
+            console.log(data)
             setApplication(data)
+            setLoading(false)
         }
         else {
             props.history.push('/dashboard')
@@ -33,13 +39,13 @@ const MyApplication = (props) => {
     }, [])
 
     return <div className='card text-center'>
-
+        {/* <img src={process.env.PUBLIC_URL + '/noimage.png'} alt="" /> */}
         <h1>{application.status}</h1>
         <h1>{application.name}</h1>
         <h1>{application.occupation}</h1>
         <h1>{application.number}</h1>
         <h1>{application.address}</h1>
-        <h1>{application.date.slice(0, 10)}</h1>
+        {loading ? <h1>Loading...</h1> : <h1>{application.date.slice(0, 10)}</h1>}
     </div>
 
 }
